1e8dc83696f6104ea8671120ae05a2aac1252e35
workvictor <workvictor@yandex.ru>
2017-12-05 23:49:58 +0300

    feat add allow to clear edit on save

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,10 @@
 import { keyMirror } from './keyMirror';
 
 export const ACTIONS = {
-  addTask: payload => dispatch => dispatch({ type: TYPES.ADD_TASK, payload }),
+  addTask: payload => dispatch =>{
+    dispatch({ type: TYPES.EDIT_CLEAR })
+    dispatch({ type: TYPES.ADD_TASK, payload })
+  },
   editTask: payload => dispatch => dispatch({ type: TYPES.EDIT_TASK, payload }),
   removeTask: payload => dispatch => dispatch({ type: TYPES.REMOVE_TASK, payload }),
   loadFromStorage: payload => dispatch => dispatch({ type: TYPES.LOAD_FROM_STORAGE, payload }),
@@ -10,6 +13,7 @@ export const ACTIONS = {
 export const TYPES = keyMirror({
   ADD_TASK: null,
   EDIT_TASK: null,
+  EDIT_CLEAR: null,
   REMOVE_TASK: null,
   LOAD_FROM_STORAGE: null,
 });
