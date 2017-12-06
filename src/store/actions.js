5f9e41deff29dac8822bfae7450dde61c0590a61
workvictor <workvictor@yandex.ru>
2017-12-06 08:43:18 +0300

    feat add new actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,19 +1,46 @@
 import { keyMirror } from './keyMirror';
 
+const storage = window.localStorage;
+
+const saveToStorage = (taskList, field = 'active') => {
+  storage.setItem(field, JSON.stringify(taskList));
+};
+
 export const ACTIONS = {
-  addTask: payload => dispatch =>{
-    dispatch({ type: TYPES.EDIT_CLEAR })
-    dispatch({ type: TYPES.ADD_TASK, payload })
+  addTask: payload => (dispatch, getStore) => {
+    dispatch({ type: TYPES.EDIT_CLEAR });
+    dispatch({ type: TYPES.ADD_TASK, payload });
+    saveToStorage(getStore().task);
   },
-  editTask: payload => dispatch => dispatch({ type: TYPES.EDIT_TASK, payload }),
-  removeTask: payload => dispatch => dispatch({ type: TYPES.REMOVE_TASK, payload }),
-  loadFromStorage: payload => dispatch => dispatch({ type: TYPES.LOAD_FROM_STORAGE, payload }),
+  editTask: payload => (dispatch, getStore) => {
+    payload.id !== undefined
+      ? dispatch({ type: TYPES.EDIT_TASK_BY_ID, payload })
+      : dispatch({ type: TYPES.EDIT_TASK, payload });
+    saveToStorage(getStore().task);
+  },
+  editTaskByID: payload => (dispatch, getStore) => {
+    dispatch({ type: TYPES.EDIT_TASK_BY_ID, payload });
+    saveToStorage(getStore().task);
+  },
+  completeTask: payload => (dispatch, getStore) => {
+    dispatch({ type: TYPES.COMPLETE_TASK, payload });
+    saveToStorage(getStore().task);
+    saveToStorage(getStore().completed, 'completed');
+  },
+  removeTask: payload => (dispatch, getStore) => {
+    dispatch({ type: TYPES.REMOVE_TASK, payload });
+    saveToStorage(getStore().task);
+  },
+  loadFromStorage: payload => dispatch =>
+    dispatch({ type: TYPES.LOAD_FROM_STORAGE, payload })
 };
 
 export const TYPES = keyMirror({
   ADD_TASK: null,
   EDIT_TASK: null,
+  EDIT_TASK_BY_ID: null,
   EDIT_CLEAR: null,
+  COMPLETE_TASK: null,
   REMOVE_TASK: null,
-  LOAD_FROM_STORAGE: null,
+  LOAD_FROM_STORAGE: null
 });
--- src/store/task.js
+++ src/store/task.js
@@ -5,7 +5,6 @@ export const task = (
       title: 'This is your very first task',
       body: 'Start working by typing new task in the field above',
       id: 0,
-      isCompleted: false,
       color: 'hsla(0, 100%, 100%, 1)',
     }
   ],
@@ -16,14 +15,28 @@ export const task = (
       ...state.slice(),
       {
         ...payload,
-        id: state[state.length-1].id+1
+        id: state.length >0 ? state[state.length-1].id+1 : 0
       }
     ]
-  : type === TYPES.REMOVE_TASK
+  : type === TYPES.COMPLETE_TASK
   ? [
       ...state.slice(0, state.findIndex(elem=>elem.id===payload.id)),
       ...state.slice(state.findIndex(elem=>elem.id===payload.id)+1),
     ]
   : type === TYPES.LOAD_FROM_STORAGE
-  ? [ ...payload ]
+  ? [ ...payload.active ]
+  : type === TYPES.REMOVE_TASK
+  ? [
+      ...state.slice(0, state.findIndex(elem=>elem.id===payload.id)),
+      ...state.slice(state.findIndex(elem=>elem.id===payload.id)+1),
+    ]
+  : type === TYPES.EDIT_TASK_BY_ID
+  ? [
+    ...state.slice(0, state.findIndex(elem=>elem.id===payload.id)),
+      {
+        ...state.find(elem=>elem.id===payload.id),
+        ...payload,
+      },
+    ...state.slice(state.findIndex(elem=>elem.id===payload.id)+1),
+  ]
   : state;
