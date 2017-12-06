d72b450945709cded64308f11c65c34cf9e25992
workvictor <workvictor@yandex.ru>
2017-12-06 08:42:48 +0300

    feat  add import/export

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { ACTIONS } from '../store';
+export { Task } from './Task';
+export { TextInput, StyledInput, StyledTextArea } from './TextInput';
+export { Button } from './Button';
+export { ColorPicker } from './ColorPicker';
+export { HSLA } from './HSLA';
