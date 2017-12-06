2e58ec38309f8f7709e3d54c264ab2dd5fd672ba
workvictor <workvictor@yandex.ru>
2017-12-06 08:45:06 +0300

    feat add new reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,10 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { task } from './task';
 import { edit } from './edit';
+import { completed } from './completed'
 
 const AppData = combineReducers({
   task,
   edit,
+  completed,
 });
 export const STORE = createStore(
   AppData,
